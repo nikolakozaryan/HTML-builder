1e1ed2997543f134d1dc74a8fe461bfe712b1e8e
Nikolay <kolyak770@gmail.com>
2022-05-22 21:19:39 +0300

    refactor: refactor first task code

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,6 @@
-const fs = require('fs'),
-      path = require('path');
+const fs = require("fs"),
+  path = require("path");
 
-const input = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+const input = fs.createReadStream(path.join(__dirname, "text.txt"), "utf-8");
 
-input.on('data', chunk => {
-    process.stdout.write(chunk);
-})
+input.on("data", (chunk) => process.stdout.write(chunk));
